26d0d005d14c09bf255b0b1ea196f9be013f70f7
andi-neck <andi.neck@intesso.com>
2015-09-04 11:37:39 +0200

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# glint-plugin-wrap-i18n
+
+
+adds `locale` function to the server and prefixes the containers id's with the `locale` + '-'
+
+
+# install
+
+```bash
+npm install glint-plugin-wrap-i18n
+```
+
+# use
+
+> This Module is part of [glintcms](http://glintcms.com/).
+> Please see the [documentation](https://github.com/glintcms/glintcms) for more info.
+
+
+# test
+
+```bash
+npm test
+```
+
+# license
+
+MIT
+
+> sponsored by [intesso](http://intesso.com)
---
+++ index.js
@@ -0,0 +1,172 @@
+/**
+ * Module dependencies.
+ */
+
+var c = require('glint-i18n/config');
+var defaults = require('defaults');
+var isBrowser = require('is-browser');
+var debug = require('debug')('glint:plugin-wrap-i18n');
+
+/**
+ *  Wrap locale Plugin
+ *
+ * - adds `locale`, (alias: `setLocale`), and `getLocale` function to the wrap
+ * - attaches `pre-load` and `pre-save` handlers to the containers, to prefix the id with the locale.
+ * - attaches `pre-save` event to the containers adapters, to add the fields: `locale`, `path`.
+ *
+ */
+module.exports = function(o) {
+  o = defaults(o, c);
+  o.locale = o.locale || 'locale';
+  o.path = o.path || 'path';
+  o.pattern = o.pattern || '([a-zA-Z]{1,3}(?:-(?:[a-zA-Z0-9]{2})){0,1})';
+
+  var attribute = o.attribute || 'locale';
+
+  plugin.api = 'wrap-plugin';
+  function plugin(wrap) {
+
+    function attachHandlers(containers) {
+
+      debug('attachHandlers containers.length', containers.length);
+
+      containers.forEach(function(container) {
+
+        // container load handler
+        container.on('pre-load', function() {
+          var locale = wrap.getLocale();
+          var id = container.id();
+          debug('pre-load container.id():', id, ', locale:', locale);
+          if (!locale ||!id) return;
+          if (id.indexOf(locale) == 0) return; // id starts with locale
+          container.id(locale + '-' + id);
+        });
+
+        // container save handler
+        container.on('pre-save', function() {
+          var locale = wrap.getLocale();
+          var id = container.id();
+          debug('pre-save container.id():', id, ', locale:', locale);
+          if (!locale ||!id) return;
+          if (id.indexOf(locale) == 0) return; // id starts with locale
+          container.id(locale + '-' + id);
+        });
+
+        // adapter save handler
+        var adapter = container.adapter();
+        if (!adapter) return;
+
+        adapter.on('pre-save', function() {
+          var args = [].slice.apply(arguments);
+          var len = args.length, pos = 3;
+          if (len <= pos) return debug('missing argument');
+          var obj = args[pos]; // object: 4th argument
+          var id = args[pos - 1]; // id: 3rd argument
+
+          //extract locale from id e.g. de-ch-home-alone -> de-ch
+          var locale;
+          var pattern = new RegExp('^' + o.pattern + '', 'i');
+          if (pattern.test(id)) {
+            locale = RegExp.$1;
+          }
+          if (locale) {
+            obj[o.locale] = locale;
+            obj[o.path] = id.replace(locale + '-', '');
+
+          }
+          debug('adatper pre-save, locale:', obj[o.locale], ', path:', obj[o.path]);
+        });
+
+      });
+
+    }
+
+    /**
+     *
+     * adds Locale Setter function.
+     * chainable function.
+     *
+     * @param value Locale like `de-CH`.
+     * @returns {Object} self
+     */
+    wrap.locale = wrap.setLocale = function(value) {
+      debug('wrap setLocale', value);
+      if (typeof value !== 'undefined') {
+        wrap['_' + attribute] = value;
+        wrap.flow.forEach(function(key, ctrl) {
+          if (typeof ctrl[attribute] === 'function') {
+            debug('wrap flow', key, ctrl);
+            ctrl[attribute](value);
+          }
+        });
+      }
+      return wrap;
+    };
+
+    /**
+     * adds Locale Getter function.
+     *
+     * @returns {String} Locale like `en` or `en-GB`.
+     */
+    wrap.getLocale = function() {
+      debug('wrap getLocale', wrap['_' + attribute]);
+      return wrap['_' + attribute];
+    };
+
+    var handlers = false;
+    wrap.on('pre-load', function() {
+
+      // attach container save handler once
+      // the handlers are attached on the wrap's pre-load event,
+      // because the containers might not be available during this plugins instatiation.
+      if (!handlers) attachHandlers(wrap.containers);
+      handlers = true;
+
+    });
+
+    wrap.on('post-load', function(content) {
+      debug('post-load', o.translate, !isBrowser, typeof wrap.translate !== 'undefined');
+      if (!isBrowser && o.translate && content && typeof content[o.translate] === 'string') {
+
+        if (!wrap.translate) return;
+
+        // get the content to translate
+        var text = content[o.translate];
+
+        // translate i18n-* strings
+        var translated = wrap.translate(text);
+        debug('translate', text);
+        debug('translated', translated);
+
+        // write back the translated content
+        content[o.translate] = translated || text;
+
+      }
+    })
+
+    // TODO browser: make translations editable (inline) e.g. with i18n="<key>"
+
+    /**
+     * initializes wrap for i18n.
+     *
+     * @param i18n internationalization object with properties: locale, locales, translate
+     * @returns {wrap} Object
+     */
+    wrap.i18n = function(i18n) {
+      debug('i18n', i18n.locale, i18n.locales, typeof i18n.translate !== 'undefined');
+      if (!i18n) {
+        console.error('no i18n object provided', i18n);
+        return wrap;
+      }
+      wrap.setLocale(i18n.locale);
+      wrap.locales = i18n.locales;
+      wrap.translate = i18n.translate;
+
+      return wrap;
+    }
+
+  }
+
+  return plugin;
+
+};
